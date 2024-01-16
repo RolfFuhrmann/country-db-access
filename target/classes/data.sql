f9c40d36146797867714eee577cae98392b85372
Rolf Fuhrmann <75734625+RolfFuhrmann@users.noreply.github.com>
2024-01-16 12:24:54 +0100

    Delete target/classes directory

--- target/classes/data.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS countries (id INT NOT NULL AUTO_INCREMENT, name VARCHAR(255), continent VARCHAR(255),  PRIMARY KEY (id));
-INSERT INTO countries (id, name, continent) VALUES (1, 'USA', 'Nordamerika' );
-INSERT INTO countries (id, name, continent) VALUES (2, 'Canada','Nordamerika');
-INSERT INTO countries (id, name, continent) VALUES (3, 'Mexico', 'Südamerika');
-INSERT INTO countries (id, name, continent) VALUES (4, 'Brazil', 'Südamerika');
-INSERT INTO countries (id, name, continent) VALUES (5, 'Germany', 'Europa');
-INSERT INTO countries (id, name, continent) VALUES (6, 'France', 'Europa');
-INSERT INTO countries (id, name, continent) VALUES (7, 'Spain', 'Europa');
-INSERT INTO countries (id, name, continent) VALUES (8, 'Portugal', 'Europa');
